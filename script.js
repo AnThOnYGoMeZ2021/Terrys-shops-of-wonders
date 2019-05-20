7ebca90c3ce2f2e70929e89908609aa76171c2e4
AnThOnYGoMeZ2021 <43757747+AnThOnYGoMeZ2021@users.noreply.github.com>
2019-05-20 09:11:41 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+
+$("#gloves").click(function(){
+   $("#yashin").slideToggle(); 
+});
+    
+$("#GOD").click(function(){
+    $("#messi").slideToggle();
+
+     
+    
+
+});
+
+$("#yay").click(function(){
+    $("#strength").fadeToggle();
+
+});
+
+$("#hey").click(function(){
+    $("#hello").slideToggle();
+
+});
